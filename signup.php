b317393482dd4ec5f4c281d8bd4dd1ad7f292e58
Eyinadepeter <itemize6@gmail.com>
2023-06-29 14:04:13 +0100

    new add

--- signup.php
+++ signup.php
@@ -7,13 +7,46 @@ include_once "header.php"
     <section class="sign_up">
         <h2>Sign up</h2>
         <div class="signup">
-            <form action="controller/sign_up.php" method="post">
-                <input type="text" name="name" placeholder="enter your full name">
+            <form action="controller/sign_up.php" method="post" class="signup-form">
+                <!-- <input type="text" name="name" placeholder="enter your full name">
                 <input type="email" name="email" placeholder="enter a valid email">
                 <input type="text" name="FullName" placeholder="username">
                 <input type="password" name="pwd" placeholder="password">
                 <input type="password" name="pwdrepeat" placeholder="repeat password">
-                <button type="submit" name="submit">Sign up</button>
+                <button type="submit" name="submit">Sign up</button> -->
+
+         <div class="register">
+        
+            
+        <label> Name:<input type="text" name="name" placeholder="Enter name" /></label>
+        
+        <label>
+          Email:
+          <input type="email" name="email" placeholder="mize@example.com" />
+        </label>
+        <label>
+          FullName:
+          <input type="text" name="FullName" placeholder="Enter your FullName" />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            name="pwd"
+            placeholder="input your password"
+          />
+        </label>
+        <label>
+          Password-Repeat:
+          <input
+            type="password"
+            name="pwdrepeat"
+            placeholder="Repeat password"
+          />
+        </label>
+
+        <button type="submit" name="submit">Regiter</button>
+      </div>
             </form>
         </div>
 
